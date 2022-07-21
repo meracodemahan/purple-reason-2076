41288849821fa4d6c7121e89f4e114ee9742653e
Yash Gabhud <yash.gabhud119@gmail.com>
2022-07-21 16:30:52 +0530

    Added head navbar-yash

---
+++ headNavbar/headNavbar.js
@@ -0,0 +1,9 @@
+
+
+
+let searchBtn = document.querySelector(".bi-search")
+
+searchBtn.addEventListener("click", ()=>{
+    let searchBox = document.getElementById("searchInput")
+    searchBox.focus();
+})
