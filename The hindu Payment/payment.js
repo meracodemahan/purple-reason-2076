6b1f87921fcb3cbed9ff2acd0c9d398c73939810
vikas Maurya <101526768+Deathwalker00@users.noreply.github.com>
2022-07-23 11:19:43 +0530

    payment page

---
+++ The hindu Payment/payment.js
@@ -0,0 +1,15 @@
+let closep =document.querySelector("#closepayment")
+
+let pcontainer=document.querySelector(".paymentcontainer")
+closep.addEventListener("click" ,function(){
+    pcontainer.style.display = "none";
+})
+
+
+
+
+
+let nextp =()=>{
+
+    location.href="finalpayment.html"
+}
